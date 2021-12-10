bec72baf79176d7c4e5f9f2999905fbfebb4aad2
ytbarekg <yhailu@miu.edu>
2021-12-10 17:48:22 -0600

    init

---
+++ src/main/java/edu/miu/webstorebackend/WebstoreBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.webstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebstoreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebstoreBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/miu/webstorebackend/WebstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.webstorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebstoreBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
