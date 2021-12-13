1a5586cf4cc383d9f8da51651188a45523e121ce
henok <hhgebreslassie@miu.edu>
2021-12-13 13:48:04 -0600

    Added TokenRefreshException for handling invalid refresh tokens and expired refresh tokens

--- src/main/java/edu/miu/webstorebackend/exception/NotFoundDTO.java
+++ src/main/java/edu/miu/webstorebackend/exception/NotFoundDTO.java
@@ -1,10 +1,14 @@
 package edu.miu.webstorebackend.exception;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class NotFoundDTO {
     private String name;
     private String message;
